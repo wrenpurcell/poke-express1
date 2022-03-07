9cb6b4f67f9b0e2122d99e2f2277058c5ddd9105
ABCarrigan <addisonbcarrigan@gmail.com>
2022-03-06 20:15:52 -0500

    Show view shows req.params.id

--- server.js
+++ server.js
@@ -9,9 +9,11 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!')
 })
-
 app.get('/pokemon', (req, res) => {
     res.render('index')
+})
+app.get('/pokemon/:id', (req, res) => {
+    res.send(req.params.id)
 })
 
 app.listen(port,() => {
